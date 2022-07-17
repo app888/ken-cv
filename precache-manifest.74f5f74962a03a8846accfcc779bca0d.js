7a9c26bf6655c90d787fa961946e274f13b1462e
Kenneth Yeung <kenneth.sin.yeung@accenture.com>
2022-07-17 17:08:48 +0200

    Updates

---
+++ precache-manifest.74f5f74962a03a8846accfcc779bca0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3594d10f0674a714473",
+    "url": "/ken-cv/static/css/main.3c89011d.chunk.css"
+  },
+  {
+    "revision": "e3594d10f0674a714473",
+    "url": "/ken-cv/static/js/main.e3594d10.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/ken-cv/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "69a899466b90820c88cb",
+    "url": "/ken-cv/static/css/2.610a6e15.chunk.css"
+  },
+  {
+    "revision": "69a899466b90820c88cb",
+    "url": "/ken-cv/static/js/2.69a89946.chunk.js"
+  },
+  {
+    "revision": "5f6838c71ece058d051a",
+    "url": "/ken-cv/static/js/runtime~main.5f6838c7.js"
+  },
+  {
+    "revision": "b5594cf82447e91b2f4c23638247df32",
+    "url": "/ken-cv/static/media/work.b5594cf8.svg"
+  },
+  {
+    "revision": "238ddd0e4c771c0151461780d61bf894",
+    "url": "/ken-cv/index.html"
+  }
+];
